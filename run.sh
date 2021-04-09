4a3813ea98b9fa2aa15ef76ed595cdc7a7041fd0
Dirk Heilig <heilig@adesso-mobile.de>
2021-04-09 19:50:37 +0200

    dont use suto to build

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo docker build -t dehydrated_hetzner_wildcard_cert .
+docker build -t dehydrated_hetzner_wildcard_cert .
 
 docker run --rm -it -v "$PWD/config:/etc/dehydrated" -v "$PWD/bundle:/bundle" -v "$PWD/data:/var/lib/dehydrated" dehydrated_hetzner_wildcard_cert /usr/local/bin/drun.sh
