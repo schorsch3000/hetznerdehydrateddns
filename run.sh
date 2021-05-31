350b05c9dc58a068ede22014b5140b83af1517bb
Dirk Heilig <heilig@adesso-mobile.de>
2021-05-31 12:51:45 +0200

    remove -i

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build -t dehydrated_hetzner_wildcard_cert .
 
-docker run --rm -it -v "$PWD/config:/etc/dehydrated" -v "$PWD/bundle:/bundle" -v "$PWD/data:/var/lib/dehydrated" dehydrated_hetzner_wildcard_cert /usr/local/bin/drun.sh
+docker run --rm -t -v "$PWD/config:/etc/dehydrated" -v "$PWD/bundle:/bundle" -v "$PWD/data:/var/lib/dehydrated" dehydrated_hetzner_wildcard_cert /usr/local/bin/drun.sh
